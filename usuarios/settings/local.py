22764e3b3bc9ca75f9b5f82d3ced175f2ad50933
Jar Cristhian <jarcrixx@gmail.com>
2021-05-25 16:27:21 -0500

    Configuracion

---
+++ usuarios/settings/local.py
@@ -0,0 +1,25 @@
+from .base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
+
+
+# Rutas para archivos staticos
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+# Rutas para archivos multimedia
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
